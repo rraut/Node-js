a11e4434a80f5f1565c50f94c8725f142053e6ca
rraut <rraut@equalexperts.com>
2016-12-01 12:32:40 +0530

    Added ejs

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get("/",function(request,response){
-	response.send("It's been 5 years!");
-	response.end();
+	response.render('home',{
+		title : "first pagssse",
+		pages : ["home","about"]
+	});
 });
 app.get("/colors/:favoritecolor?",function(request,response){
 	var favoritecolor = request.params.favoritecolor;
 	response.send("your favorite color is " + favoritecolor);
 });
 
-
+app.get("*",function(request,response){
+	response.send("Sorry ! This page does not exist");
+});
 app.listen(3000,function(){
 	console.log("started!");
 })
